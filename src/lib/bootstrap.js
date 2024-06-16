8a77fc7c835fb94d85569b5158fca66ae6edfa26
nicky48 <nickyuri48@proton.me>
2024-06-16 10:44:49 -0300

    refactor: add removeInactiveBumpers

--- src/lib/bootstrap.js
+++ src/lib/bootstrap.js
@@ -1,10 +1,9 @@
 import { env } from "../env.js";
 import { registerEvents } from "./register-events.js";
-import { getUsers } from "../utils/get-users.js";
-import { resetUserBumpCount } from "../utils/reset-user-bump-count.js";
 import cron from "node-cron";
 import * as Sentry from "@sentry/node";
 import { $import } from "./$import.js";
+import { removeInactiveBumpers } from "../utils/remove-inactive-bumpers.js";
 
 /** @type {import('../types/commands').Commands} */
 export const commands = new Map();
@@ -25,61 +24,11 @@ export const bootstrap = async (client) => {
 
     await client.login(env.TOKEN);
 
-    const checkInId = Sentry.captureCheckIn({
-      monitorSlug: "remove-role-from-inactive-users",
-      status: "in_progress",
+    cron.schedule("0 0 * * *", now => removeInactiveBumpers(client, now), {
+      name: "remove-role-from-inactive-users",
+      timezone: "America/Sao_Paulo",
+      recoverMissedExecutions: true
     });
-
-    cron.schedule(
-      "0 0 * * *",
-      async (now) => {
-        try {
-          if (!(now instanceof Date)) return;
-
-          const limit = new Date(now.getTime() - 1000 * 60 * 60 * 24);
-
-          const users = await getUsers();
-
-          for (const user of users) {
-            if (!user.lastBumpAt) continue;
-            if (new Date(user.lastBumpAt).getTime() >= limit.getTime())
-              continue;
-
-            const guild = client.guilds.cache.get(env.GUILD_ID);
-
-            if (!guild) continue;
-
-            const member = guild.members.cache.get(user.id);
-            const bumperRole = guild.roles.cache.get(env.BUMPER_ROLE_ID);
-
-            if (!member || !bumperRole) continue;
-
-            await member.roles.remove(bumperRole, "inatividade");
-
-            await resetUserBumpCount(member.user.id);
-          }
-
-          Sentry.captureCheckIn({
-            checkInId,
-            monitorSlug: "remove-role-from-inactive-users",
-            status: "ok",
-          });
-        } catch (e) {
-          Sentry.captureException(e);
-
-          Sentry.captureCheckIn({
-            checkInId,
-            monitorSlug: "remove-role-from-inactive-users",
-            status: "error",
-          });
-        }
-      },
-      {
-        name: "remove-role-from-inactive-users",
-        timezone: "America/Sao_Paulo",
-        recoverMissedExecutions: true,
-      }
-    );
   } catch (e) {
     Sentry.captureException(e);
   }
