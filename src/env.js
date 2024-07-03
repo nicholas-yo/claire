6622a647ba48929078048ca8354c59d6934e2a9a
nicky48 <nickyuri48@proton.me>
2024-07-03 20:35:57 -0300

    fix: load env with dotenv

--- src/env.js
+++ src/env.js
@@ -1,10 +1,16 @@
 import { z } from "zod";
+import { config } from "dotenv";
+
+config({
+  path: process.env.NODE_ENV === "development" ? ".development.env" : ".env"
+});
 
 const envSchema = z.object({
   APPLICATION_ID: z.string(),
   BUMPER_ROLE_ID: z.string(),
   EXPERIENCE_ROLES: z.string().transform(arg => arg.split(",")),
   GUILD_ID: z.string(),
+  NODE_ENV: z.literal("production").or(z.literal("development")),
   SENTRY_DSN: z.string().url(),
   TOKEN: z.string(),
   TURSO_AUTH_TOKEN: z.string(),
